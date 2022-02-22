e18dfd045f723815d5fea452b099c6a08d4d7a51
saurabh kamble <saurabh2836@gmail.com>
2022-02-22 16:29:27 +0530

    Adding new features learned in code section

---
+++ lecture-one/learn-imports.py
@@ -0,0 +1,19 @@
+"""
+
+- Modules get used all the time throughout
+programming
+- IT helps with creating more files,
+with unique purposes,to help with
+clean maintainable code.
+"""
+
+import grade_service as grade_service
+
+homework_assignment_grades = {
+    'homework_1':85,
+    'homework_2':100,
+    'homework_3':81
+}
+
+
+grade_service.calculate_homework(homework_assignment_grades)
